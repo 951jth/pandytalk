95b23590f0c6eb8bb51ecad56e9dcf35c51a4062
951jth@delphicom.net <951jth@delphicom.net>
2025-12-07 21:13:00 +0900

    chore: version up

---
+++ app/repositories/chatRepository.ts
@@ -0,0 +1,17 @@
+import {doc, updateDoc} from '@react-native-firebase/firestore'
+import {firestore} from '../store/firestore'
+import type {ChatListItem} from '../types/chat'
+
+export default {
+  updateChatRoom: async (
+    roomId: string,
+    roomData: Partial<Omit<ChatListItem, 'id'>>,
+  ) => {
+    try {
+      const chatDocRef = doc(firestore, 'chats', roomId)
+      await updateDoc(chatDocRef, roomData)
+    } catch (e) {
+      console.log(e)
+    }
+  },
+}
