fddc44153f20577fa39ccd740b86eb2189b15c61
csh <951jth@delphicom.net>
2025-12-08 22:57:32 +0900

    chore: ChatListScreen 정리

--- app/features/chat/screens/ChatListScreen.tsx
+++ app/features/chat/screens/ChatListScreen.tsx
@@ -1,5 +1,5 @@
 import {useRoute, type RouteProp} from '@react-navigation/native'
-import React, {useMemo} from 'react'
+import React from 'react'
 import {FlatList, StyleSheet, View} from 'react-native'
 import COLORS from '../../../constants/color'
 // import {updateChatLastReadCache} from '../hooks/useInfiniteQuery'
@@ -15,7 +15,7 @@ type ChatRouteParams = RouteProp<AppRouteParamList, 'chats'>
 //1:1 (DM), 그룹채팅(group) 모두 사용중인 화면.
 export default function ChatListScreen() {
   const {params} = useRoute<ChatRouteParams>()
-  const type = useMemo(() => params?.type ?? 'dm', [])
+  const type = params?.type ?? 'dm'
   const {
     input,
     setInput,
@@ -28,28 +28,32 @@ export default function ChatListScreen() {
     moveToChatRoom,
   } = useChatListScreen(type)
   return (
-    <View style={{flex: 1}}>
-      <SearchInput
-        placeholder="검색할 닉네임을 입력해주세요."
-        value={input}
-        onChangeText={setInput}
-      />
+    <View style={styles.container}>
       <FlatList
         data={chats}
         keyExtractor={e => e?.id}
         renderItem={({item}) => {
-          const findMember = item?.findMember
-          const targetId = findMember?.id
+          const targetId = item?.findMember?.id
           return (
             <ChatListItemCard
               item={item}
-              onPress={item => moveToChatRoom(targetId, item.id)}
+              onPress={() => {
+                if (!targetId) return
+                moveToChatRoom(targetId, item.id)
+              }}
             />
           )
         }}
         onEndReached={() => {
           if (hasNextPage) fetchNextPage()
         }}
+        ListHeaderComponent={
+          <SearchInput
+            placeholder="검색할 닉네임을 입력해주세요."
+            value={input}
+            onChangeText={setInput}
+          />
+        }
         ListFooterComponent={
           isFetchingNextPage ? (
             <ActivityIndicator size="small" color={COLORS.primary} />
@@ -91,70 +95,9 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 8,
   },
-  frame: {
-    width: 55,
-    height: 55,
-    borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: COLORS.primary,
-    borderWidth: 1,
-    position: 'relative',
-  },
-  image: {
-    width: 50,
-    height: 50,
-    borderRadius: 8,
-  },
-  point: {
-    backgroundColor: '#2CC069',
-    width: 14,
-    height: 14,
-    borderRadius: 100,
-    borderColor: '#FFF',
-    borderWidth: 2,
-    position: 'absolute',
-    right: -6,
-    top: -6,
-  },
   contents: {
     flex: 1,
     gap: 4,
-  },
-  name: {
-    fontSize: 14,
-    color: '#000',
-    fontFamily: 'BMDOHYEON',
-  },
-  lastMessage: {
-    fontSize: 12,
-    color: '#ADB5BD',
-    fontFamily: 'BMDOHYEON',
-  },
-  lastSendTime: {
-    fontSize: 12,
-    color: '#ADB5BD',
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    fontFamily: 'BMDOHYEON',
-  },
-  unreadMessageBadge: {
-    position: 'absolute',
-    bottom: 0,
-    right: 0,
-    width: 24,
-    height: 24,
-    borderRadius: 12,
-    alignItems: 'center',
-    justifyContent: 'center', // ✅ 세로 중앙
-    backgroundColor: COLORS.primary,
-  },
-  unreadMessage: {
-    color: COLORS.onPrimary,
-    fontSize: 12,
-    fontWeight: 'bold',
-    fontFamily: 'BMDOHYEON',
   },
   empty: {
     flex: 1,
